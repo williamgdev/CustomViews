79fa51e944c6ef0d25257a1426df0939a837f84c
Will Gutierrez <williamg.dev@gmail.com>
2018-06-20 21:59:45 -0700

    Change Created CustomEdittext

---
+++ app/src/main/java/com/adnroid/devcuba/messengerproject/customview/CustomEditText.java
@@ -0,0 +1,11 @@
+package com.adnroid.devcuba.messengerproject.customview;
+
+import android.content.Context;
+import android.support.design.widget.TextInputEditText;
+import android.util.AttributeSet;
+
+public class CustomEditText extends TextInputEditText {
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
